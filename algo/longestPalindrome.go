922ce60ef8f758bb1ac86bb9613cf400c964def8
JasonHe-WQ <wenqinghe1016@gmail.com>
2023-09-30 15:31:08 +0800

    update: algo

---
+++ algo/longestPalindrome.go
@@ -0,0 +1,43 @@
+package main
+
+func longestPalindrome(s string) string {
+	n := len(s)
+	if n < 2 {
+		return s
+	}
+
+	dp := make([][]bool, n)
+	for i := range dp {
+		dp[i] = make([]bool, n)
+	}
+
+	start, maxLen := 0, 1
+
+	// All substrings of length 1 are palindromes
+	for i := 0; i < n; i++ {
+		dp[i][i] = true
+	}
+
+	// Check for substrings of length 2
+	for i := 0; i < n-1; i++ {
+		if s[i] == s[i+1] {
+			dp[i][i+1] = true
+			start = i
+			maxLen = 2
+		}
+	}
+
+	// Check for substrings of length greater than 2
+	for length := 3; length <= n; length++ {
+		for i := 0; i < n-length+1; i++ {
+			j := i + length - 1
+			if s[i] == s[j] && dp[i+1][j-1] {
+				dp[i][j] = true
+				start = i
+				maxLen = length
+			}
+		}
+	}
+
+	return s[start : start+maxLen]
+}
